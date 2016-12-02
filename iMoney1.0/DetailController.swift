1cb24b1b41019e2a6122d416bfe60c690b4c100a
Chong Zhou <lvmike@lvmikes-macbook-pro.local>
2016-12-02 03:10:08 -0600

    blank detail added

---
+++ iMoney1.0/DetailController.swift
@@ -0,0 +1,14 @@
+//
+//  HistoryController.swift
+//  iMoney1.0
+//
+//  Created by 吕Mike on 12/1/16.
+//  Copyright © 2016 文静. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import CoreLocation
+
+class DetailController: UIViewController {
+}
